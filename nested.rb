de5588cd2773c69b8eead6e94a9df613b945ed21
Katie Le <lethikatie@gmail.com>
2020-07-17 04:44:13 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -85,7 +85,7 @@ def adding_matz
      programmer_hash[:yukihiro_matsumoto] = {}
      programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
      programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
-     return programmer_hash[:yukihiro_matsumoto]
+     return :yukihiro_matsumoto
 end
 
 def changing_alan
