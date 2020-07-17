ed53ed1ff4ab4b02291045f76037765ef21dc070
Katie Le <lethikatie@gmail.com>
2020-07-17 05:28:02 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -125,10 +125,10 @@ def adding_to_dennis
         },
         :dennis_ritchie => {
           :known_for => "Unix",
-          :languages => ["C"]
+          :languages => ["C", "Assembly"]
         }
      }
-  programmer_hash[:dennis_ritchie][:languages] = 'Assembly'
+  programmer_hash[:dennis_ritchie][:languages] = "Assembly"
   return programmer_hash
   
 end
